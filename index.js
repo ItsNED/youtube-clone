bd11dd1047fe87cc63fbb61fc2a4460e9e2b89e0
Tackhee Han <taki1984@naver.com>
2019-02-25 01:27:35 +0900

    #2.7 setup cookie-parser and body-parser

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -18,6 +20,9 @@ const handleProfile = (req, res) => {
   res.send("This is your Profile");
 };
 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
